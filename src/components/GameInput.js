032d6ce524c1e435239f27c77a046d443e643669
Krigan112 <romanet.matthias@gmail.Com>
2020-03-03 09:58:11 +0100

    created the game input, created the champions list, and made the input recognise each champion when the correct name is entered

---
+++ src/components/GameInput.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GameInput = ({setGuess}) => {
+
+    return (
+        <InputContainer>
+            <StyledLabel>
+                <span>Champion name</span>
+                <input
+                    type='text'
+                    onChange={event => setGuess(event.target.value)}
+                />
+            </StyledLabel>
+        </InputContainer>
+    );
+};
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  align-items: center;
+  position:fixed;
+  z-index: 2;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+
+const StyledLabel = styled.label`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  text-align: left;
+  align-self: flex-start;
+  justify-content: space-between;
+  margin: 5px 0;
+  color: black;
+  align-items: center;
+  & input {
+    margin-left: 50px;
+    padding: 5px;
+    background-color: white;
+    border: none;
+  }
+`;
+
+export default GameInput;
